056b1a5c2c0757063eef34a200dc1343aa74d4ad
jovin <1028124611@qq.com>
2017-04-05 15:11:01 +0800

    add client test

---
+++ src/test/java/application/ConfigTest.java
@@ -0,0 +1,39 @@
+package application;
+
+import application.uil.JsonHelper;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.cluster.health.ClusterIndexHealth;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+/**
+ * Created by huangzebin on 2017/4/5.
+ */
+public class ConfigTest {
+
+    @Test
+    public void testClient() throws UnknownHostException {
+        ClusterHealthResponse clusterHealthResponse =
+                getClient().admin().cluster().health(new ClusterHealthRequest("")).actionGet();
+        Map<String, ClusterIndexHealth> indices = clusterHealthResponse.getIndices();
+        System.out.println(JsonHelper.toJSON(indices));
+    }
+
+    public static TransportClient getClient() throws UnknownHostException {
+        Settings settings = Settings.builder()
+                .put("cluster.name", "htbaobao").build();
+        return new PreBuiltTransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.1.88"), 9300));
+    }
+
+
+
+}
