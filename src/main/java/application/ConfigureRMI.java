cafac5d41c93f55e87e05f7e29b0b4e7cb1d68ab
jovin <1028124611@qq.com>
2017-05-19 17:26:48 +0800

    add rmi

---
+++ src/main/java/application/ConfigureRMI.java
@@ -0,0 +1,40 @@
+package application;/**
+ * Created by huangzebin on 2017/5/19.
+ */
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jmx.support.ConnectorServerFactoryBean;
+import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
+
+@Configuration
+public class ConfigureRMI {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Value("${jmx.rmi.host:localhost}")
+    private String rmiHost;
+
+    @Value("${jmx.rmi.port:1099}")
+    private Integer rmiPort;
+
+    @Bean
+    public RmiRegistryFactoryBean rmiRegistry() {
+        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
+        rmiRegistryFactoryBean.setPort(rmiPort);
+        rmiRegistryFactoryBean.setAlwaysCreate(true);
+        return rmiRegistryFactoryBean;
+    }
+
+    @Bean
+    @DependsOn("rmiRegistry")
+    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
+        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
+        connectorServerFactoryBean.setObjectName("connector:name=rmi");
+        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
+        return connectorServerFactoryBean;
+    }
+}
